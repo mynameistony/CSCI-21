fce0c3c5a56abf2ce9bb661c53681145ad3a773d
OpenShift guest <tonyrogers10195@gmail.com>
2014-01-27 12:48:36 -0500

    Changes to be committed:    (use "git rm --cached <file>..." to unstage) 	new file:   pc1.cpp

---
+++ pc1.cpp
@@ -0,0 +1,129 @@
+/*
+ * Programming Challenge 1
+ */
+#include <cassert>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string greet (string name);
+string checkExperience (char hasExperience);
+
+/* for unit testing -- do not alter */
+template <typename X, typename A>
+void btassert(A assertion);
+void unittest (string s, char c);
+
+int main (int argc, char* argv[])
+{
+
+string userName;
+
+char programmedBefore;
+
+cout << "\nHello, user!";
+
+cout << "\nWhat is your name?\n";
+
+cin >> userName; 
+
+cout << "\nHave you programmed before? ('y' or 'n')\n";
+
+cin >> programmedBefore;
+
+	unittest(userName, programmedBefore);
+	
+	return 0;
+}
+
+/*
+ * Create a string greeting that is the concatenation of a message 
+ * and a name.
+ * @param name a string containing a user name
+ * @return the string "Nice to meet you, NAME" where NAME contains 
+ *         the parameter value
+ */
+string greet (string name)
+{
+	string message = "Nice to meet you, ";
+	
+	message += name;
+	//strcat(message,name);
+	
+	return message;
+}
+
+/*
+ * Create a string message based upon whether or not a user has C++ 
+ * programming experience.
+ * @param hasExperience a char ('Y'es or 'N'o) representing whether or 
+ *        not a user has C++ programming experience
+ * @return "Welcome back" when hasExperience is 'Y', else 'Get ready to 
+ *         have some fun"
+ */
+string checkExperience (char hasExperience)
+{
+    string message;
+    
+	if (hasExperience == 'y')
+	message = "Welcome back";
+    else if (hasExperience == 'n')
+	message = "Get ready to have some fun";
+	else
+	cout << "You dun goof'd, try again";
+	
+	return message;
+
+}
+
+/*
+ * Unit test. Do not alter this function.
+ */
+void unittest (string s, char c)
+{
+	if (s == "fez")
+	{
+		cout << "\nSTARTING UNIT TEST\n\n";
+		
+		try {
+			btassert<bool>(greet(s) == "Nice to meet you, fez");
+			cout << "Passed TEST 1: greet\n";
+		} catch (bool b) {
+			cout << "# FAILED TEST 1 greet #\n";
+		}
+		
+		if (toupper(c) == 'Y')
+		{
+			try {
+				btassert<bool>(checkExperience(c) == "Welcome back");
+				cout << "Passed TEST 2: checkExperience\n";
+			} catch (bool b) {
+				cout << "# FAILED TEST 2 checkExperience #\n";
+			}
+		}
+		else if (toupper(c) == 'N')
+		{
+			try {
+				btassert<bool>(checkExperience(c) == "Get ready to have some fun");
+				cout << "Passed TEST 2: checkExperience\n";
+			} catch (bool b) {
+				cout << "# FAILED TEST 2 checkExperience #\n";
+			}
+		}
+		
+		cout << "\nUNIT TEST COMPLETE\n\n";
+	}
+	else
+	{
+		cout << greet(s) << endl;
+		cout << checkExperience(c) << endl;
+		cout << "\nRun program with username \"fez\" to see unit test output.\n";
+	}
+}
+
+template <typename X, typename A>
+void btassert (A assertion)
+{
+    if (!assertion)
+		throw X();
+}
